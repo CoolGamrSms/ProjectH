60a813e055d30ee04f3c386176f5802d273c5f73
Shane Schulte <schulte.shane@gmail.com>
2014-11-21 22:05:29 -0500

    Initial commit

---
+++ android/src/com/shaneschulte/projecth/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.shaneschulte.projecth.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.shaneschulte.projecth.ProjectH;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectH(), config);
+	}
+}
---
+++ desktop/src/com/shaneschulte/projecth/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shaneschulte.projecth.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shaneschulte.projecth.ProjectH;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectH(), config);
+	}
+}
