d670a02894f2fd3f4de30c810fc34140736542b7
Shane Schulte <schulte.shane@gmail.com>
2014-11-22 03:25:33 -0500

    Started work  on GameScreen

--- android/src/com/shaneschulte/projecth/android/AndroidLauncher.java
+++ android/src/com/shaneschulte/projecth/android/AndroidLauncher.java
@@ -4,10 +4,8 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.parse.ParseObject;
-import com.shaneschulte.projecth.ProjectH;
-
 import com.parse.Parse;
+import com.shaneschulte.projecth.ProjectH;
 
 public class AndroidLauncher extends AndroidApplication {
     private final String clientKeyA = "wMO59oRluIVwH0MtU5AKofKubqo0qgirZRQCh2bF";
@@ -22,6 +20,8 @@ public class AndroidLauncher extends AndroidApplication {
 
         //Initializes android configurations
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useAccelerometer = false;
+        config.useCompass = false;
 		initialize(new ProjectH(), config);
 	}
 }
--- core/src/com/shaneschulte/projecth/ProjectH.java
+++ core/src/com/shaneschulte/projecth/ProjectH.java
@@ -1,27 +1,23 @@
 package com.shaneschulte.projecth;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.shaneschulte.projecth.com.shaneschulte.projecth.screens.GameScreen;
 
-public class ProjectH extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class ProjectH extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    @Override
+    public void create() {
+        Gdx.app.log("ProjectH", "created");
+
+        batch = new SpriteBatch();
+        //Use LibGDX's default Arial font.
+        font = new BitmapFont(Gdx.files.internal("monkey.fnt"),Gdx.files.internal("monkey.png"),false);
+        setScreen(new GameScreen(this));
+    }
 }
